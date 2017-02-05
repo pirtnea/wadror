c0c484ecd96eec5f51c7d723ed6bfd0504a660f9
Nea Pirttinen <nea.pirttinen@cs.helsinki.fi>
2017-02-05 02:22:35 +0200

    Validations and error messages

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
