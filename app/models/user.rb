06743fcc64aa440d06d4b02785400299a9e02b26
Nea Pirttinen <nea.pirttinen@cs.helsinki.fi>
2017-02-05 03:26:29 +0200

    Proper authentication and editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
   include RatingAverage
 
+  has_secure_password
+
   validates :username, uniqueness: true, length: { in: 3..30 }
+  validates :password, length: { minimum: 4 }, format: { with: /.*(\d.*[A-Z]|[A-Z].*).*/ }
+
+  has_many :ratings, dependent: :destroy
+  has_many :beers, through: :ratings
 
   has_many :ratings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204233250) do
+ActiveRecord::Schema.define(version: 20170205003821) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20170204233250) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
