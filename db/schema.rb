efd952000b8a1d4fb381b594fe79e3c263894d25
Nea Pirttinen <nea.pirttinen@cs.helsinki.fi>
2017-02-26 22:20:50 +0200

    Added admins to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225234726) do
+ActiveRecord::Schema.define(version: 20170226195839) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170225234726) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
